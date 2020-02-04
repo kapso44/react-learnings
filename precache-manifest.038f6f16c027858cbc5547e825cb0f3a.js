87d0a8cf4c963c5062de4e71bb4aa6e9b172b019
Kapil sharma <kapil@targetbay.com>
2020-02-04 18:56:18 +0530

    Updates

---
+++ precache-manifest.038f6f16c027858cbc5547e825cb0f3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12ed801d2062d6c9da28771f7aaaa7fb",
+    "url": "/react-learnings/index.html"
+  },
+  {
+    "revision": "dad79e57b703d6fb8d99",
+    "url": "/react-learnings/static/css/main.103d9fb2.chunk.css"
+  },
+  {
+    "revision": "46b7bbb9ca53fca1153e",
+    "url": "/react-learnings/static/js/2.5c706983.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-learnings/static/js/2.5c706983.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dad79e57b703d6fb8d99",
+    "url": "/react-learnings/static/js/main.c0007546.chunk.js"
+  },
+  {
+    "revision": "638713f20372d75956db",
+    "url": "/react-learnings/static/js/runtime-main.2580ffca.js"
+  }
+]);
